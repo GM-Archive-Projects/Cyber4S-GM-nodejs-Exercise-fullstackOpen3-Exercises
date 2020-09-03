0b3b75c55bf6dee472e4af92a087f420ac0a001e
MrMoshkovitz <gal@gmail.com>
2020-09-03 13:41:31 +0300

    a

---
+++ client/build/precache-manifest.c321f0039347f1c0ba54c8f5561182cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9639781f95d1a9833a22d66435b62f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02f6301ef4ac1f735bae",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "62e9756b378e88dbd8de",
+    "url": "/static/js/2.19dbbced.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19dbbced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f6301ef4ac1f735bae",
+    "url": "/static/js/main.0dff87ba.chunk.js"
+  },
+  {
+    "revision": "0d1bd1bbc8101739a13d",
+    "url": "/static/js/runtime-main.0d702a3c.js"
+  }
+]);
